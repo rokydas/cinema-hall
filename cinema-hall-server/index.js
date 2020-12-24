3864b8f1809bf58bf9e23fcdd2ab1c88f1b24c3d
rokydas <droky136@gmail.com>
2020-12-24 17:55:03 +0600

    mongodb problem solved

--- cinema-hall-server/index.js
+++ cinema-hall-server/index.js
@@ -1,13 +1,47 @@
-const express = require('express'); 
-const bodyParser = require('body-parser'); 
+const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const app = express(); 
+const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.get('/', (req, res) => {
-        res.send('Hello I am your new node js project');
-})
+require('dotenv').config()
+
+const user = process.env.USER;
+const password = process.env.PASSWORD
+const db = process.env.DB;
+
+const MongoClient = require('mongodb').MongoClient;
+const { ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${user}:${password}@cluster0.muwip.mongodb.net/${db}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+
+        const movies = client.db("cinema-hall").collection("movies");
+
+        app.get('/', (req, res) => {
+                res.send('Hello I am your new node js project');
+        })
+
+        app.get('/movies', (req, res) => {
+                movies.find({})
+                        .toArray((err, documents) => {
+                                res.send(documents);
+                        })
+        })
+
+        // app.post('/insert', (req, res) => {
+        //         movies.insertOne(req.body)
+        //                 .then(result => {
+        //                         res.send(result.insertedCount > 0)
+        //                 })
+        // })
+
+});
+
+
+
 app.listen(process.env.PORT || 5000);
